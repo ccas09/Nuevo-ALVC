7a657895d87a9c299c45fc8f4d1dc295b0625b96
Almendra <almendrareinosomansilla@gmail.com>
2020-09-23 22:12:05 -0300

    nuevos cambios cultura y espectaculo

--- Desktop/Nuevo-ALVC-master/src/app/app-routing.module.ts
+++ Desktop/Nuevo-ALVC-master/src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { EspectaculosComponent } from './espectaculos/espectaculos.component';
-
+import { CulturalesComponent } from './culturales/culturales.component'; 
 ;
 
 const routes: Routes = [
 {path : 'Inicio', component: InicioComponent},
 {path : 'Registrate', component: RegistrarComponent},
 { path: 'Espectaculo', component: EspectaculosComponent },
-{path : '**', component: InicioComponent}
+{path : 'Culturales', component: CulturalesComponent},
+{path : '**', component: InicioComponent},
 ];
 
 @NgModule({
